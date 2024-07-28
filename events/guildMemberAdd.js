47ec4d008880d3ce2ac7f1fe06f4e49b81f3159f
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-07-28 17:05:44 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,10 +4,8 @@ const path = require('path');
 const { Wcard } = require('wcard-gen');
 const data = require('../UI/banners/welcomecards');
 
-
 const configPath = path.join(__dirname, '../config.json');
 let config = {};
-
 
 function loadConfig() {
     try {
@@ -18,14 +16,12 @@ function loadConfig() {
     }
 }
 
-
 loadConfig();
-
 
 setInterval(loadConfig, 5000);
 
 function getOrdinalSuffix(number) {
-    if (number === 11 || number === 12 || number === 13) {
+    if ([11, 12, 13].includes(number % 100)) {
         return 'th';
     }
     const lastDigit = number % 10;
@@ -50,12 +46,11 @@ module.exports = (client) => {
         console.log(`New member added: ${member.user.tag} in guild: ${member.guild.name}`);
 
         const guildId = member.guild.id;
-        const settings = config.guilds[guildId];
+        const settings = config.guilds ? config.guilds[guildId] : null;
 
         if (settings && settings.status) {
             const welcomeChannel = member.guild.channels.cache.get(settings.welcomeChannelId);
             if (welcomeChannel) {
-              
                 const memberCount = member.guild.memberCount;
                 const suffix = getOrdinalSuffix(memberCount);
                 const userName = member.user.username;
@@ -63,46 +58,48 @@ module.exports = (client) => {
                 const creationDate = member.user.createdAt.toDateString();
                 const serverName = member.guild.name;
                 const serverIcon = member.guild.iconURL({ format: 'png', dynamic: true, size: 256 });
-
-               
+                
                 const randomImage = getRandomImage(data.welcomeImages);
 
-                
-                const welcomecard = new Wcard()
-                    .setName(userName)
-                    .setAvatar(member.user.displayAvatarURL({ format: 'png' }))
-                    .setTitle("Welcome to Server")
-                    .setColor("00e5ff") 
-                    .setBackground(randomImage);
-                
-                const card = await welcomecard.build();
-                const attachment = new AttachmentBuilder(card, { name: 'welcome.png' });
+                try {
+                    const welcomecard = new Wcard()
+                        .setName(userName)
+                        .setAvatar(member.user.displayAvatarURL({ format: 'png' }))
+                        .setTitle("Welcome to Server")
+                        .setColor("00e5ff")
+                        .setBackground(randomImage);
 
-                const embed = new EmbedBuilder()
-                    .setTitle("Welcome to the Server!")
-                    .setDescription(`${member}! You are the **${memberCount}${suffix}** member of our server!`)
-                    .setColor("#00e5ff")
-                    .setThumbnail(member.user.displayAvatarURL())
-                    .setImage('attachment://welcome.png')
-                    .addFields(
-                        { name: 'Username', value: userName, inline: true },
-                        { name: 'Join Date', value: joinDate, inline: true },
-                        { name: 'Account Created', value: creationDate, inline: true }
-                    )
-                    .setFooter({ text: "We're glad to have you here!", iconURL: serverIcon })
-                    .setAuthor({ name: serverName, iconURL: serverIcon })
-                    .setTimestamp();
+                    const card = await welcomecard.build();
+                    const attachment = new AttachmentBuilder(card, { name: 'welcome.png' });
 
-                welcomeChannel.send({
-                    content: `Hey ${member}!`,
-                    embeds: [embed],
-                    files: [attachment]
-                });
+                    const embed = new EmbedBuilder()
+                        .setTitle("Welcome to the Server!")
+                        .setDescription(`${member}! You are the **${memberCount}${suffix}** member of our server!`)
+                        .setColor("#00e5ff")
+                        .setThumbnail(member.user.displayAvatarURL())
+                        .setImage('attachment://welcome.png')
+                        .addFields(
+                            { name: 'Username', value: userName, inline: true },
+                            { name: 'Join Date', value: joinDate, inline: true },
+                            { name: 'Account Created', value: creationDate, inline: true }
+                        )
+                        .setFooter({ text: "We're glad to have you here!", iconURL: serverIcon })
+                        .setAuthor({ name: serverName, iconURL: serverIcon })
+                        .setTimestamp();
+
+                    welcomeChannel.send({
+                        content: `Hey ${member}!`,
+                        embeds: [embed],
+                        files: [attachment]
+                    });
+                } catch (error) {
+                    console.error('Error generating or sending welcome card:', error);
+                }
             } else {
                 console.log(`Welcome channel not found for guild: ${member.guild.name}`);
             }
         } else {
-            console.log(`Welcome messages are disabled for guild: ${member.guild.name}`);
+            console.log(`Welcome messages are disabled or settings not found for guild: ${member.guild.name}`);
         }
     });
 };
